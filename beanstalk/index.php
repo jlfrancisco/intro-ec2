80fff2691b89df893c2666a64b036cea06bebd18
Jean Luc F SEMEDO <jsemedo_ct@securiport.com>
2022-11-11 08:23:53 -0500

    Beanstalk files demo added

---
+++ beanstalk/index.php
@@ -0,0 +1,1 @@
+<html><head><title>Hello World</title></head><body><h1 align="center">Hello World! Version 1.0</h1><br><div align="center"><a href="#"><img src="JeanLucSEMEDO.jpeg"></a></div></body></html>
